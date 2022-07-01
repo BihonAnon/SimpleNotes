42a084290c494b5e59ea2d88d523a165c0730f38
byron <byrondalberg@gmail.com>
2022-07-01 09:56:14 -0700

    start script add

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON anherd urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
+
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -25,7 +25,7 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
-
+app.use('/api', apiRoutes);
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 )
